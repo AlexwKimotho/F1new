557ca3d7aa9bcd3a9901e5a5a21d9c587b07498d
alexwachirakimotho <lexkimothowachira@gmail.com>
2023-10-08 11:29:59 +0300

    Landing page demo

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Landing from "./landingpage"
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <div className="container">
-      <div className="card">
-        <div className="card-image">
-           <p style={{ fontWeight: 'bold', margin: '10px'}}> Featured</p>
-          <p className="card-text"> <br/>Audi torn on Engine Deal for Red Bull Racing</p>
-         
-        </div>
-      </div>
-
-      <div className="card-2 update-card">
-        <div className="card2-title">
-          <h2>Latest Updates</h2>
-          
-        </div>
-      </div>
-    </div>
-  
-
+      <Landing/>
       <Footer />
       {/* Other components/content goes here */}
     </div>
---
+++ pages/landingpage.js
@@ -0,0 +1,84 @@
+import React from "react";
+
+const Landing = () => {
+    return (
+<div className="container col-xxl-8 col-xl-10 col-lg-6">
+        <div className="card">
+          <div className="card-image">
+            <p style={{ fontWeight: "Bold", margin: "10px", color: "Grey" }}>
+              {" "}
+              Featured
+            </p>
+            <p
+              style={{
+                fontWeight: "bold",
+                marginTop: "270px",
+                marginLeft: "10px",
+              }}
+            >
+              Audi torn on Engine Deal for Red Bull Racing
+            </p>
+            <p style={{ fontSize: "Medium", margin: "10px" }}>
+              Toto wolf has admitted supplying Red Bull with engines could be
+              "an attractive option" for Mercees. It is a significant
+              change.....
+            </p>
+          </div>
+        </div>
+
+        <div className="col-xxl-6 col-xl-4 col-lg-6">
+          <div className="card2">
+            <div className="card2-header">
+              <h4 className="card-title">Top Stories</h4>
+              <div className="unpaid-content">
+                <ul>
+                  <li>
+                    <p className="mb-0">August 20, 2015</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="card2-header">
+              <h6 className="card-title">
+                Vettel will do anything to 'annoy mercedes
+              </h6>
+            </div>
+            <div className="card2-header">
+              <h6 className="card-title">
+                Renault continued to work hard during summer break
+              </h6>
+            </div>
+            <div className="card2-header">
+              <h6 className="card-title">
+                A lap Spa-Francorchamps onboard with Rosberg
+              </h6>
+            </div>
+            <div className="card2-header">
+              <h6 className="card-title">
+                Clampdown on starts 'step in the right direction' -
+              </h6>
+            </div>
+            <div className="card2-header">
+              <h6 className="card-title">
+                Ferrari re-signs Kimi Raikkonen for 2016
+              </h6>
+            </div>
+            <div className="card2-body">
+              <div className="unpaid-content">
+                <ul>
+                  <li>
+                    <p className="mb-0">August 19, 2015</p>
+                  </li>
+                  <li>
+                    <p className="mb-0">August 19, 2015</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+        );
+    };
+    
+    export default Landing;
