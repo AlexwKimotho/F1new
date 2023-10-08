4b19f4c0a7145dea313850cf42ebe1e9d229a68b
alexwachirakimotho <lexkimothowachira@gmail.com>
2023-10-08 17:19:58 +0300

    News car

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import Landing from "./landingpage"
+import Landing from "./landingpage";
+import Standings from "./standings";
+import NewsCard from "./newsCard"
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <Landing/>
+      <Landing />
+      
+    <Standings/>
+    <NewsCard/>
       <Footer />
       {/* Other components/content goes here */}
     </div>
---
+++ pages/newsCard.js
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+
+const NewsCard = ({ title, image, description }) => (
+  <div className="col-md-3 mb-4">
+    <div className="card2 h-200">
+      <img src={image} className="card-img-top" alt={title} />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+      </div>
+    </div>
+  </div>
+);
+
+const BigCard = ({ title, children }) => (
+  <div className="card2 mb-23 col-xl-6">
+    <div className="card-body">
+      <h5 className="card-title">{title}</h5>
+      <div className="row">{children}</div>
+    </div>
+  </div>
+);
+
+const Home = () => {
+  return (
+    <div className="container">
+      <BigCard title="News">
+        <NewsCard
+          title="News Title 1"
+          image="image_url_1.jpg"
+          description="Description for News 1"
+        />
+        <NewsCard
+          title="News Title 2"
+          image="image_url_2.jpg"
+          description="Description for News 2"
+        />
+        <NewsCard
+          title="News Title 3"
+          image="image_url_3.jpg"
+          description="Description for News 3"
+        />
+      </BigCard>
+
+     
+    </div>
+  );
+};
+
+export default Home;
