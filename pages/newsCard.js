46c5a832de44fa0d6dc18533c9e58e855aa0a204
alexwachirakimotho <lexkimothowachira@gmail.com>
2023-10-08 23:51:28 +0300

    pushed newsCard updates

--- pages/newsCard.js
+++ pages/newsCard.js
@@ -4,7 +4,7 @@ const News = () => {
     return (
         <div className="row m-4 ">
        <div className="col-xl-5 p-2" style={{ marginTop: "-200px" }}>
-        <div className="card2"style={{ height: '490px' }}>
+        <div className="card2"style={{ height: '670px' }}>
           <div className="card2-header">
             <h4 className="card-title">News, Opinion And Analysis</h4>
           </div>
@@ -69,7 +69,7 @@ const News = () => {
                         className="img-fluid" // Added class to make the image fluid
                       />
                       <br/>
-                      <strong> Ex-F1racer Jarno Trulli Steps down from racing in Formula E </strong>
+                      <strong> McLaren F1 team's Jenson Button calls BelgianGP </strong>
                     </span>
                   </div>
                   
@@ -85,7 +85,7 @@ const News = () => {
                         className="img-fluid" // Added class to make the image fluid
                       />
                       <br/>
-                      <strong> Ex-F1racer Jarno Trulli Steps down from racing in Formula E </strong>
+                      <strong> Pierre Galy surprised by Daniel de Jong move before GP2 crash </strong>
                     </span>
                   </div>
                   
@@ -101,7 +101,55 @@ const News = () => {
                         className="img-fluid" // Added class to make the image fluid
                       />
                       <br/>
-                      <strong> Ex-F1racer Jarno Trulli Steps down from racing in Formula E </strong>
+                      <strong> Justin Wilson airlifted to hospital after ponoco IndyCar crash </strong>
+                    </span>
+                  </div>
+                  
+                </div>
+              </div>
+              <div className="col-xl-4">
+                <div className="stat-widget d-flex align-items-center">
+                  <div className="widget-icon ">
+                    <span>
+                      <img
+                        src="https://live-production.wcms.abc-cdn.net.au/ea9071af8e0e3a392578d6f4db435cd1?impolicy=wcms_crop_resize&cropH=1376&cropW=2447&xPos=0&yPos=65&width=862&height=485"
+                        alt="First Image"
+                        className="img-fluid" // Added class to make the image fluid
+                      />
+                      <br/>
+                      <strong> BTCC suspends one of Rob Austin Racing's TOCA BTCC licences </strong>
+                    </span>
+                  </div>
+                  
+                </div>
+              </div>
+              <div className="col-xl-4">
+                <div className="stat-widget d-flex align-items-center">
+                  <div className="widget-icon ">
+                    <span>
+                      <img
+                        src="https://live-production.wcms.abc-cdn.net.au/ea9071af8e0e3a392578d6f4db435cd1?impolicy=wcms_crop_resize&cropH=1376&cropW=2447&xPos=0&yPos=65&width=862&height=485"
+                        alt="First Image"
+                        className="img-fluid" // Added class to make the image fluid
+                      />
+                      <br/>
+                      <strong> FIA forced to clarify Forula 1 radio clampdown ambiguities </strong>
+                    </span>
+                  </div>
+                  
+                </div>
+              </div>
+              <div className="col-xl-4">
+                <div className="stat-widget d-flex align-items-center">
+                  <div className="widget-icon ">
+                    <span>
+                      <img
+                        src="https://live-production.wcms.abc-cdn.net.au/ea9071af8e0e3a392578d6f4db435cd1?impolicy=wcms_crop_resize&cropH=1376&cropW=2447&xPos=0&yPos=65&width=862&height=485"
+                        alt="First Image"
+                        className="img-fluid" // Added class to make the image fluid
+                      />
+                      <br/>
+                      <strong> Former Formula 1 team boss and racing driver Guy Liger dies </strong>
                     </span>
                   </div>
                   
